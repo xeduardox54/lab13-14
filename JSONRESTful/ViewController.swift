bb8f59830a2dc86a1ff5d01e8d24f8afd7e33a39
Eduardo Rodriguez <eduardo.rodriguez@tecsup.edu.pe>
2021-06-22 12:02:01 -0700

    Initial Commit

---
+++ JSONRESTful/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JSONRESTful
+//
+//  Created by Eduardo Justo Rodriguez Herrera on 6/22/21.
+//  Copyright © 2021 Eduardo Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
